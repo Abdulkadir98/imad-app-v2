78ee58895d9fff7bfba681e3af931a78caf5c5a7
Abdulkadir98 <ak98.narutokun@gmail.com>
2017-02-12 12:39:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
-console.log('Loaded!');
+//code for button click
+var button = document.getElementById("Counter");
+var counter = 0;
+button.onclick = function(){
+    counter = counter + 1;
+    var span = document.getElementById("Count");
+    span.innerHTML = counter.toString();
+    
+};
