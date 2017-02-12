dbdd80376e20bfc37926018499fd6dca0120c69a
Abdulkadir98 <ak98.narutokun@gmail.com>
2017-02-12 13:04:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,5 @@
 //code for button click
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function(){
     
     var request = new XMLHttpRequest();
@@ -8,7 +7,7 @@ button.onclick = function(){
         
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
-                counter = request.responseText;
+                var counter = request.responseText;
                 var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
     
